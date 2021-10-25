d589e09a4487e48e932297ae1ff4a4cdcce6bf5c
iliashad <iliasshaddad18@gmail.com>
2021-10-25 15:52:54 +0100

    Update revalidation time

--- pages/product/[handle].js
+++ pages/product/[handle].js
@@ -27,7 +27,7 @@ export async function getStaticProps({ params }) {
     // Next.js will attempt to re-generate the page:
     // - When a request comes in
     // - At most once every 5 seconds
-    revalidate: 5,
+    revalidate: 1,
   };
 }
 
